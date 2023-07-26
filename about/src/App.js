9138dc3f92e30e81d35915953c7cfdfb5699821c
Asad Subhani <asadsubhani77@gmail.com>
2023-07-26 20:48:35 +0500

    App implemented some dark them type functionality to a secction

--- about/src/App.js
+++ about/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="About Us"/>
+    <div className="container-sm my-3" >    
+    <About/>
+
     </div>
+    
+
+    </>
   );
 }
 
