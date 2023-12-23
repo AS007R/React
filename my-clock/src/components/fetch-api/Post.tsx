b313dbe96cfe75d3e663633cbf9925e46704743a
Asad Subhani <asadsubhani77@gmail.com>
2023-12-23 10:39:15 +0500

    make some changes in fetch request

--- my-clock/src/components/fetch-api/Post.tsx
+++ my-clock/src/components/fetch-api/Post.tsx
@@ -10,20 +10,48 @@ type post = {
 };
 
 const Post = () => {
-  const [posts, setPosts] = useState<post[]>([]);
-  const [filterPosts, setFilterPosts] = useState<post[]>(posts);
-  fetch("https://dummyjson.com/posts")
-    .then((res) => res.json())
-    .then((data) => setPosts(data.posts));
-  console.log(filterPosts);
-  function handleOnClick(id: number) {
-    const newFilterPosts = posts.filter((item) => id != item.id);
-    setFilterPosts(newFilterPosts);
-  }
+  const [posts, setPosts] = useState<post[]>([
+    {
+      id: 1,
+      title: "His mother had always taught him",
+      body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+      userId: 9,
+      tags: ["history", "american", "crime"],
+      reactions: 2,
+    },
+    {
+      id: 2,
+      title: "His mother had always taught him",
+      body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+      userId: 9,
+      tags: ["history", "american", "crime"],
+      reactions: 2,
+    },
+    {
+      id: 3,
+      title: "His mother had always taught him",
+      body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+      userId: 9,
+      tags: ["history", "american", "crime"],
+      reactions: 2,
+    },
+    {
+      id: 4,
+      title: "His mother had always taught him",
+      body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+      userId: 9,
+      tags: ["history", "american", "crime"],
+      reactions: 2,
+    },
+  ]);
 
+  // fetch("https://dummyjson.com/posts")
+  //   .then((res) => res.json())
+  //   .then((data) => setPosts(data.posts));
+  //  currently getting error in fetching data "too many request error"
   return (
     <>
-      {filterPosts.map((item) => (
+      {posts.map((item) => (
         <div key={item.id} className="modal-content rounded-4 shadow p-3 w-25">
           <div className="modal-header border-bottom-0">
             <h1 className="modal-title fs-5">{item.title}</h1>
@@ -32,7 +60,6 @@ const Post = () => {
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
-              onClick={() => handleOnClick(item.id)}
             ></button>
           </div>
           <div className="modal-body py-0">
